e221e1ffc5ac3d9b5ffc72383140e5a0525e68b6
daybet <daybetlee@gmail.com>
2020-04-21 22:33:14 +0800

    First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D theRB;
+    public float moveSpeed;
+
+    public Joystick joystick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(joystick.Horizontal, joystick.Vertical) * moveSpeed;
+    }
+}
